bcb1cf67d806c99b6ae5bde07978f6f6f569dc86
carillo-hub <79423468+carillo-hub@users.noreply.github.com>
2021-11-11 16:12:10 -0500

    Add files via upload

--- MPC_only.m
+++ MPC_only.m
@@ -4,86 +4,93 @@
 global Tfin N x0 xfin tmpc time 
 Tfin = 10;
 N = 12;
-x0 = [11,0, 0, 0, -1.5, 0, 0, 0];
-xfin = [11, 0, 0, 0, 0, 0, 0, 0];
-e = 0;
-phi_init = [0, 0.32, 1.63, 4.98];
+Z0 = [11,0, 0, 0, -1.5, 0, 0, 0];
+phi_init = [0, 0, 0, 0.2];
 tmpc = 0.1;
 time = N*tmpc;
-seq_u = ones(1,N) * 4;
-winit = 0;
-thetainit = 0;
-lb = seq_u*-5;
-ub = seq_u*5; 
-%% 
+
+% Optimal Controller I.C 
+seq_u_e_init = ones(1,N+1);
+seq_u_init = seq_u_e_init(1,1:N);
+winit = 0; thetainit = 0;
+[w0, theta0] = Steering(seq_u_init, winit, thetainit, phi_init);
+Steer_seq0 = [w0; theta0];
+Z_seq_init = Z_pred(Steer_seq0, Z0')
+
+%constraints 
+lb = zeros(1,N+1); ub = zeros(1,N+1);
+lb(1,1:N) = -5;
+ub(1,1:N) = 5; 
+lb(1,N+1) = 0;
+ub(1,N+1) = inf ;
+global dTmin dTmax
+dTmax = 10;
+dTmax = 10;
+
+
+%% Optimal Control problem --> Minimization
 %-------------------------------------------------
-% Optimal Control problem --> Minimization
-%-------------------------------------------------
-u_optimal = fmincon(@(seq_u)Minim(Z_seq, seq_u, e), seq_u, [], [], [], [], lb, ub )
-[w_opt, theta_opt] = Statespacerep(u_optimal, winit, thetainit, phi_init);
+nonlcon = @dU_bounds ;
+u_e_optimal = fmincon(@(seq_u_e)Minim(Z_seq_init, seq_u_e(:,1:N), seq_u_e(:,N+1)), seq_u_e_init, [], [], [], [], lb, ub, nonlcon );
+u_opt = u_e_optimal(:,1:N)
+e_opt = u_e_optimal(:,N+1)
+[w_opt, theta_opt] = Steering(u_optimal, winit, thetainit, phi_init);
 Steer_seq = [w_opt; theta_opt]
-Z_opt = Z_pred(Steer_seq, x0')
+Z_opt = Z_pred(Steer_seq, Z0')
 Cost = Minim(Z_opt, u_optimal, e)
 
-
+% Plot results 
 figure(1)
 t=[0:tmpc:tmpc*(N-1)];
 plot(t, Z_opt(5,:),'x','MarkerEdgeColor','black')
 grid on 
+hold on
+plot(t,u_opt)
+hold on 
+scatter(t,e_opt,'filled')
 title('Lateral deviation from lane center line')
 xlabel('time')
 ylabel('ey')
+ylim([-2,2])
+legend('ey','u_seq','e')
+hold off 
 
-%----------------------------------------------
-% Optima Control problem --> One iteration 
-%----------------------------------------------
-%Switch = 1;
-%global Tfin x0 ;
-%[w, theta] = Statespacerep(seq_u, winit, thetainit, phi_init);
-%Steer_seq = [w; theta];
-%Z_seq = Z_pred(Steer_seq, x0');
-%Cost = Minim(Z_seq, seq_u, e);
-%Switch = Switch.*-1;
+% non linear constraints 
+function [c,ceq] = dU_bounds(seq_u_e)
+global N dTmin dTmax 
+% Generate dU from seq_u
+for ctr = 2:1:N
+    dU(:,ctr) = seq_u_e(:,ctr) - seq_u_e(:,ctr-1); 
+end
+c1 = dU - dTmax;
+c2 = dTmin - dU;
+c = c1 + c2;
+ceq = [];
 
-%%
+end 
+
+%% SS to describe the system --> outputs the Steering theta and w sequence for u_seq
 %-------------------------------------------------------------------------
-% SS to describe the system --> outputs the theta and w sequence for u_seq
-%-------------------------------------------------------------------------
-function [w, theta] = Statespacerep(seq_u, wcurr, thetacurr, phi_list)
+function [w, theta] = Steering(seq_u, wcurr, thetacurr, phi_list)
 global tmpc time N
 phi1 = phi_list(1,2);
 phi2 = phi_list(1,3);
 phi3 = phi_list(1,4);
-theta = ones(1,N); w = ones(1,N);
-%u = get_seq(seq_u);                       %generate first elem of u_sequence 
+theta = ones(1,N); w = ones(1,N);                  %initialize the output arrays
 
 for ctr1 = 1:1:N
-    u = seq_u(:,ctr1)
+    u = seq_u(:,ctr1);                             %get first elem of u seq
     wnew = phi1*thetacurr + phi2*wcurr +phi3*u ;   %use it to find wnew
     thetanew = thetacurr + wnew*tmpc;              %use wnew to find thetanew
-    w(:,ctr1) = wnew;                        %get the w_seq for u_seq
-    theta(:,ctr1) = thetanew;                %get the theta_seq for u_seq
+    w(:,ctr1) = wnew;                              %get the w_seq for u_seq
+    theta(:,ctr1) = thetanew;                      %get the theta_seq for u_seq
+    thetacurr = thetanew;                          %reset thetacurr for next iter
+    wcurr = wnew;                                  %reset wcurr for next iter
 end
 
 end 
 
-
-%----------------------
-% Construct u sequence
-%----------------------
-function u = get_seq(seq_u)
-global Tfin N time
-a = N*time/Tfin ;
-if a==0
-    u = seq_u(:,1);
-else
-    u = seq_u(:,abs(ceil(a)));
-end
-end
-
-%%
-%-----------------------------------------------------------
-% Generate entire Z sequence for N prediction horizon length
+%% Generate entire Z sequence for N prediction horizon length 
 % Requires an input sequence u and the current state Z_curr 
 %  ζ =[Δx,Δy, ̇Δψ, e_̇ψ , ey , s, θ, ω].transpose
 %-----------------------------------------------------------
@@ -113,7 +120,7 @@ for ctr3 = 1:1:N
     eydot = (xdot*sin(ecactus)) + (ydot*cos(ecactus));
     ey_next = eydot*tmpc + ey;
     sdot = (xdot*cos(ecactus)) - (ydot*sin(ecactus));
-    s_next = sdot + s;
+    s_next = sdot*tmpc + s;
     head_f_next = theta(:,ctr3) ;
     w_next = w(:,ctr3);
 
@@ -137,9 +144,7 @@ end
 %end
 
 
-%%
-%-----------------------------------------------------------
-% Generate Cost 
+%%  Generate Cost 
 % Requires an input sequence u and the current state Z_curr 
 %  ζ =[Δx,Δy, ̇Δψ, e_̇ψ , ey , s, θ, ω].transpose
 %-----------------------------------------------------------
@@ -151,30 +156,22 @@ N = 12;
 Q = 15; R = 10; M = 1000;
 W = zeros(8,8); W(2,2) = 5; W(3,3) = 5; W(5,5) = 10; % Weight matrix for penalizing items in ζ 
 Tc_term = 0; Tstep = 0; Z_term = 0; Zstep = 0;
-dU = zeros(1,N); Zref = [11; 0; 0; 0; 0; 0; 0; 0;];
+dU = zeros(1,N); Zref = [0; 0; 0; 0; 0; 0; 0; 0;];
 
 % Generate dU from seq_u
 for ctr = 2:1:N
     dU(:,ctr) = seq_u(:,ctr) - seq_u(:,ctr-1); 
 end
-dU ;
-
 % generate Tc_term in Cost eq
 for i = 1:1:N-1
     Tstep = seq_u(:,i)'*Q*seq_u(:,i) + dU(:,i)'*R*dU(:,i); 
     Tc_term = Tc_term + Tstep;
 end
-Tc_term ;
-
 % generate Z_term in Cost eq
 for j = 1:1:N
     Zstep = (Z_seq(:,j) - Zref)'*W*(Z_seq(:,j) - Zref)  ;
     Z_term = Z_term + Zstep;
 end 
-Z_term;
-
 % Generate Final Cost 
 Cost = Tc_term + Z_term + (M*e);
-
 end
-%%  
